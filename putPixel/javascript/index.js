697b870e707b5d00479c3a309f597c225c9b0e56
志築孝広 <takahiro@mac.local>
2020-12-29 17:37:20 +0900

    update javascript/index.js

--- putPixel/javascript/index.js
+++ putPixel/javascript/index.js
@@ -3,12 +3,8 @@ var ctx = canvas.getContext('2d');
 
 var pixels = ctx.getImageData(0,0, canvas.width, canvas.height);
 var data = pixels.data;
-for (var i=0; i<data.length; i+=4) {   
-    data[i]   = 0;
-    data[i+1] = 0;
-    data[i+2] = 0;
-    data[i+3] = 255;
-}
+data[0]   = 255;
+data[1] = 0;
+data[2] = 0;
+data[3] = 255;
 ctx.putImageData(pixels, 0,0);
-
-console.log("did!!");
